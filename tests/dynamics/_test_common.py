481e0560d2bb4bbabc5067546b2a26aa38f1e73c
Till Hoffmann <tillahoffmann@gmail.com>
2019-07-11 12:14:36 +0100

    Add method to test for correlation.

--- tests/dynamics/_test_common.py
+++ tests/dynamics/_test_common.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pytest
 
+import graph_dynamics as gd
+
 
 def _test_integration(operator, z0=None, time=1):
     if z0 is None:
@@ -10,17 +12,16 @@ def _test_integration(operator, z0=None, time=1):
 
     # Run the integration
     z_numeric = operator.integrate_numeric(z0, time)
-    z_naive = operator.integrate_naive(z0, np.linspace(0, time, 200) if np.isscalar(time) else time)[-1]
-    corrcoef = np.corrcoef(z_naive.ravel(), z_numeric.ravel())[0, 1]
-    assert corrcoef > 0.99, "numeric and naive integration differ (correlation = %f)" % corrcoef
+    z_naive = operator.integrate_naive(z0, np.linspace(0, time, 1000) if np.isscalar(time) else time)[-1]
+    # Use slightly laxer tolerance on the offset because of the finite integration step
+    gd.assert_correlated(z_naive, z_numeric)
 
     if not operator.has_analytic_solution:
         return z0, z_numeric, None
 
     # Make sure the values match
     z_analytic = operator.integrate_analytic(z0, time)
-    corrcoef = np.corrcoef(z_analytic.ravel(), z_numeric.ravel())[0, 1]
-    assert corrcoef > 0.99, "numeric and analytic integration differ (correlation = %f)" % corrcoef
+    gd.assert_correlated(z_numeric, z_analytic)
     return z0, z_numeric, z_analytic
 
 
