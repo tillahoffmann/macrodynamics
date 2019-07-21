e54a092e10e5103bceafe3a564fd7a52acaa524c
Till Hoffmann <tillahoffmann@gmail.com>
2019-07-21 17:47:02 +0100

    Extend information in setup.py.

--- setup.py
+++ setup.py
@@ -38,8 +38,10 @@ tests_require = [
 ]
 
 setup(
-    name='macrodynamics',
+    name="macrodynamics",
     version="0.1",
+    author="Till Hoffmann",
+    url="https://github.com/tillahoffmann/macrodynamics",
     packages=find_packages(),
     ext_modules=ext_modules,
     install_requires=[
@@ -51,4 +53,12 @@ setup(
     extras_require={
         "tests": tests_require,
     },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ]
 )
